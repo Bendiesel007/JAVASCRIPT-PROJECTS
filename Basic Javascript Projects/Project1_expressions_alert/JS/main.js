69351f739c74feade6155a621ac97287a41a1c4e
Bendiesel007 <electricman4820002000@yahoo.com>
2026-06-30 14:18:55 -0700

    BASIC JAVASCRIPT submission step #65

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+// main.js
+// Sent1 and Sent2 are string values
+// window.alert is the method used
+// \n creates a line break so each variable appears on its own line
+// Concatenate the strings by utilizing +
+window.alert("Sentence 1 : " + "This is the beginning of the string" + "\nSentence 2: " + "and this is the end of the string");
+
+// window.alert expression and statement to display a result after the OK button is clicked on the alert window
+window.alert(200 * 2)
+
+// document.write an expression and statement to display a result on the page after the OK button is clicked twice on the alert window
+document.write(100 + 100);
+
+// Utilize horizontal line "<hr>" to seperate each document method result
+document.write("<hr>")
+
+// Utilize document.write method to concatenate a string using the + operator. Results display on the page after the OK button is clicked twice on the alert window
+document.write("\"this is the beginning," + "and this is the end of the string.\"")
+
+
+
+
