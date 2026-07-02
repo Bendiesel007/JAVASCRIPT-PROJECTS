b7a0b83871aceb46220e7399e63d66879e9c2075
Bendiesel007 <electricman4820002000@yahoo.com>
2026-07-01 20:38:56 -0700

    Project 2 Functions Submission Step #70

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function myfunction() { // Define a function and name it myfunction
+    var message = "I am learning how to use Javascript funtions"; // Define a variable and give
+    message += " and operators with The Tech Academy's Javascript course!"; // it a string value
+    document.getElementById("Concatenate") .innerHTML = message;  // Put the value of the variable into 
+    //the HTML element. Use += operator to concatenate a string
+}
