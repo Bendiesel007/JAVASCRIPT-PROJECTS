b594ea378b5b724fba75d121affbefda3a2e9da2
Bendiesel007 <electricman4820002000@yahoo.com>
2026-09-23 21:20:01 -0700

    AJAX RESUBMIT

--- AJAX/ajaxScript.js
+++ AJAX/ajaxScript.js
@@ -1,11 +1,26 @@
 function loadDoc() {
+  //Setup the XML HTTP request object
+
   var xhttp = new XMLHttpRequest();
+  //Instantiate a new XMLHttpRequest (XHR) object, which
+  //allows web browsers to make HTTP requests to a server
+  //without reloading the page
+
+  //Defines an event listener for an Asynchronous and XML
+  //(AJAX) request using XMLHttpRequest (XHR) object
   xhttp.onreadystatechange = function() {
+
+    //Establishes whether the response was successful and 
+    //where the data should be displayed
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("demo").innerHTML =
       this.responseText;
     }
   };
+    //Prepare the type of request and what  
+    //to request from the server
   xhttp.open("GET", "ajax_info.txt", true);
+
+    //Send the request
   xhttp.send();
 }
